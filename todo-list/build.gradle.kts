1967b2a2ad1c798a03762dcd42bb55d8930246b4
yanliwei <xhyan926@gmail.com>
2022-07-02 23:50:12 +0800

    增加aliyun国内镜像源

--- todo-list/build.gradle.kts
+++ todo-list/build.gradle.kts
@@ -6,12 +6,15 @@ group = "com.xhyan.zero"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven(uri("https://maven.aliyun.com/repository/public/"))
+    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter:2.7.1")
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.1")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:2.7.1")
     //测试依赖库
     testImplementation(platform("org.junit:junit-bom:5.8.2"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
