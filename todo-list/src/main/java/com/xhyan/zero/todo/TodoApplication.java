fc43da83d83b3c87481e0ab27a19dd91da84ef5a
yanliwei <xhyan926@gmail.com>
2022-07-02 23:23:32 +0800

    初始化项目工程
    1. 定义启动类和冒烟测试类

---
+++ todo-list/src/main/java/com/xhyan/zero/todo/TodoApplication.java
@@ -0,0 +1,7 @@
+package com.xhyan.zero.todo;
+
+public class TodoApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ todo-list/src/test/java/com/xhyan/zero/todo/SmokeTest.java
@@ -0,0 +1,12 @@
+package com.xhyan.zero.todo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SmokeTest {
+    
+    @Test
+    public void should_success_when_one_equals_one(){
+        Assertions.assertEquals(1, 1);
+    }
+}
