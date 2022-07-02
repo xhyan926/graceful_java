e4df9122fa37a4c7943acd723240c4a3d823b352
yanliwei <xhyan926@gmail.com>
2022-07-03 00:02:50 +0800

    添加springboot启动入口

--- todo-list/src/main/java/com/xhyan/zero/todo/TodoApplication.java
+++ todo-list/src/main/java/com/xhyan/zero/todo/TodoApplication.java
@@ -1,7 +1,13 @@
 package com.xhyan.zero.todo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
 public class TodoApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        SpringApplication.run(TodoApplication.class, args);
     }
 }
