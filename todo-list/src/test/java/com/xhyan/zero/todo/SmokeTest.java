078ce41efafab4f7a723d33b7125112603c1e9ba
yanliwei <xhyan926@gmail.com>
2022-07-08 23:17:55 +0800

    增加githooks 配置

--- todo-list/src/test/java/com/xhyan/zero/todo/SmokeTest.java
+++ todo-list/src/test/java/com/xhyan/zero/todo/SmokeTest.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -22,6 +21,6 @@ class SmokeTest {
     public void status_is_up_when_test_health_check() throws Exception {
         mockMvc.perform(get("/actuator/health"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value("UP"));
+                .andExpect(jsonPath("$.status").value("U1P"));
     }
 }
